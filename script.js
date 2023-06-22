b6132a884b4cd898de2c653492967f2b3bb61c2a
kalash tayal <130529290+kalashtayal@users.noreply.github.com>
2023-06-22 18:42:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //your JS code here. If required.
-let colorSelect = document.getElementById("colorSelect");
-let removeColor = document.getElementByTagName('input');
-removeColor.addEventListener("click" , function () {
-	let selectedColor= colorSelect.value;
+// let colorSelect = document.getElementById("colorSelect");
+// let removeColor = document.getElementByTagName('input');
+// removeColor.addEventListener("click" , function () {
+// 	let selectedColor= colorSelect.value;
 	
-	let selectedOption = colorSelect.querySelector('option[value"'= + selectedColor +'"]');
-  if (selectedOption) {
-    selectedOption.remove();
-  }
-});
+// 	let selectedOption = colorSelect.querySelector('option[value"'= + selectedColor +'"]');
+//   if (selectedOption) {
+//     selectedOption.remove();
+//   }
+// });
+
+function remove() {
+	const drop = document.querySelector('#colorSelect');
+	drop.remove(drop.selectedIndex);
+	
+}
